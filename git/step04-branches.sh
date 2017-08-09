14fd5475d56a8e64f94c44ac3970765b41522b37
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-08-09 23:57:29 +0300

    Create step04-branches.sh

---
+++ git/step04-branches.sh
@@ -0,0 +1,11 @@
+# Create new branch
+git checkout -b “#101”
+touch file.txt
+git add *
+git status
+git commit –m”your comment here”
+git checkout master
+
+# Push branch
+git checkout “#101”
+git push origin “#101”
