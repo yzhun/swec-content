3a78afff2eae7d2572543ea60ef159d0fe3be6c1
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-08-09 23:22:36 +0300

    Update step02-generate-ssh-key.sh

--- git/step02-generate-ssh-key.sh
+++ git/step02-generate-ssh-key.sh
@@ -1,10 +1,14 @@
 # Switch to your local machine
+
 # Go to .ssh folder
 cd ~/.ssh
+
 # Generate pair of SSH keys, use sec as key name, use empty passphrase
 ssh-keygen -t rsa -b 4096
+
 # Add SSH config file on local machine, use your remote VM ip address as <host>
 echo "Host <host>" > config
 echo "  IdentityFile ~/.ssh/sec" >> config
+
 # Copy public key to the remote VM
 scp ./sec.pub root@<host>:~/
