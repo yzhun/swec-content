362fd23ff4cd1616fd65ba2613f61ce2f107849f
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-11-08 11:55:17 +0200

    Update step-01-install.sh

--- docker/step-01-install.sh
+++ docker/step-01-install.sh
@@ -21,5 +21,5 @@ sudo apt-get update
 sudo apt-get -y install docker-ce
 
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.17.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
