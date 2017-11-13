4e83bd1709933dfca98e24529fee1ab9ff0793ed
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-11-13 18:34:26 +0200

    Update step12-subtree.sh

--- git/step12-subtree.sh
+++ git/step12-subtree.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# add subtree
+git subtree add --prefix bootstrap-subtree https://github.com/twbs/bootstrap.git master --squash
+
+# 
+git log -2
+
+# pull subtree
+git subtree pull --prefix bootstrap-subtree https://github.com/twbs/bootstrap.git master --squash
+
+# use remote
+git remote add bootstrap https://github.com/twbs/bootstrap.git
+git remote -v
+git subtree add --prefix bootstrap-subtree-from-remote bootstrap master --squash
