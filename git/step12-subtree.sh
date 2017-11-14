8f29d334f53989fc8ae3d490eac0fee8ab02921c
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-11-14 19:19:50 +0200

    Update step12-subtree.sh

--- git/step12-subtree.sh
+++ git/step12-subtree.sh
@@ -17,3 +17,18 @@ git subtree push --prefix bootstrap-subtree bootstrap-origin master
 
 
 # using read-tree
+git fetch bootstrap-upstream
+git branch -av
+
+git checkout -b bootstrap_branch bootstrap-upstream/master
+git checkout master
+git read-tree --prefix=bootstrap-read-tree/ -u bootstrap_branch
+git add *
+git commit -m"read-tree test"
+
+# pull bootstrap
+git checkout bootstrap_branch
+git pull
+
+git checkout master
+git merge --squash -s subtree --no-commit bootstrap_branch
