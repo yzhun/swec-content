697ec518099135458c5ea8c43ccd74e993ca90f2
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-11-14 19:32:46 +0200

    Update step11-cherrypick.sh

--- git/step11-cherrypick.sh
+++ git/step11-cherrypick.sh
@@ -1,1 +1,23 @@
 
+#
+##
+git checkout -b feature
+# create file 1.txt
+touch 1.txt
+git add *
+git commit -m"1.txt"
+
+# create file 2.txt
+touch 2.txt
+git add *
+git commit -m"2.txt"
+
+# create file 3.txt
+touch 3.txt
+git add *
+git commit -m"3.txt"
+
+git log -4
+# copy commit hash for "2.txt" <commit-hash>
+
+git cherry-pick <commit-hash>
