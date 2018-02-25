7deef3e924f65b06004ea1cbceb8b96e30f635a6
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-02-25 21:58:07 +0000

    Verificatin commands

--- prereq.sh
+++ prereq.sh
@@ -5,6 +5,14 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install mc
 
-echo --- Instal Talan ---
+echo --- Install Talan ---
 sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/project-talan/talan-core/latest/install.sh)"
 sudo tln install java,maven,nodejs,docker,docker-compose
+
+echo --- Check cmponents ---
+java -version
+mvn -v
+node -v
+npm -v
+docker -v
+docker-compose -v
