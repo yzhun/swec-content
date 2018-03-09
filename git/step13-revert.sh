6cd64c3ff3482530c2c47dd54ec946c91a2d6de5
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2018-03-09 15:55:31 +0200

    Update step13-revert.sh

--- git/step13-revert.sh
+++ git/step13-revert.sh
@@ -7,5 +7,11 @@ git commit -m"master.txt"
 git merge feature
 
 # exit :wq
-
 git log --graph
+
+# revert merge commit
+$ git revert HEAD
+#error: commit 86ab406d8bd5f123b4ae6438308cccb093d34616 is a merge but no -m option was given.
+
+# revert with parent from master
+git revert HEAD -m 1
