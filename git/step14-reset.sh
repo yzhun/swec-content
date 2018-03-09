17f5f532167496449bea95d9036542c9940fea81
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2018-03-09 16:13:36 +0200

    Update step14-reset.sh

--- git/step14-reset.sh
+++ git/step14-reset.sh
@@ -1,1 +1,13 @@
 
+# remove from staging area
+touch file2reset.txt
+git status
+git add *
+git status
+git reset HEAD -- .
+git status
+
+# switch to one of the commit from history
+#git reset HEAD --soft
+#git reset HEAD --mixed
+#git reset HEAD --hard
