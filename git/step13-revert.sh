9e47005b3aea10595a6546e55f01d0c217c2e143
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2018-03-16 15:01:29 +0200

    Update step13-revert.sh

--- git/step13-revert.sh
+++ git/step13-revert.sh
@@ -1,17 +1,26 @@
 
+
+# create and modify additional branch
+git checkout -b b2revert
+touch revert.txt
+git add *
+git commit -m"revert.txt"
+
+# modify master branch
 git checkout master
 touch master.txt
 git add *
 git commit -m"master.txt"
 
-git merge feature
+# merge
+git merge b2revert
 
 # exit :wq
 git log --graph
 
 # revert merge commit
 $ git revert HEAD
-#error: commit 86ab406d8bd5f123b4ae6438308cccb093d34616 is a merge but no -m option was given.
+#error: commit <SHA1> is a merge but no -m option was given.
 
 # revert with parent from master
 git revert HEAD -m 1
