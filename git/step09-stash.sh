94c73474f5595e2d209e62c334ee1e9098d29753
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2018-04-09 14:33:02 +0300

    Update step09-stash.sh

--- git/step09-stash.sh
+++ git/step09-stash.sh
@@ -1,1 +1,15 @@
+# change working directory
+touch file2stash
+git status
 
+# stach changes
+git add *
+git stash
+git stach list
+
+# switch to another branch
+git checkout 18.5.0
+git stash pop
+git commit -m"commit stash"
+
+gir checkout master
