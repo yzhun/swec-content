90bc7c64bf884fca4ac6454d1d899a75bacc3f2f
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2018-05-24 19:08:06 +0300

    Update step11-rebase.sh

--- git/step11-rebase.sh
+++ git/step11-rebase.sh
@@ -4,13 +4,13 @@ git checkout -b b2r
 
 # make changes inside b2r branch
 touch b2r
-git add *
+git add .
 git commit -m"b2r commit"
 
 # switch to master and make changes there
 git checkout master
 touch b2r.master
-git add *
+git add .
 git commit -m"commit in master"
 
 # rebase
