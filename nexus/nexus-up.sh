1785703cd7c0dedbc7845ddbb6881408679e9c8d
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2019-01-08 19:36:53 +0200

    Update nexus-up.sh

--- nexus/nexus-up.sh
+++ nexus/nexus-up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm $1 -p 8081:8081 --name nexus sonatype/nexus3
+$ mkdir /opt/nexus-data && chown -R 200 /opt/nexus-data
+$ docker run --rm $1 -p 8081:8081 --name nexus -v /opt/nexus-data:/nexus-data sonatype/nexus3
