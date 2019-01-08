6f08e266d1715d6ef04d2aa7068afb7bb1524467
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2019-01-08 18:42:10 +0200

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -6,11 +6,10 @@ sudo apt-get -y upgrade
 sudo apt-get -y install mc
 
 echo --- Install Talan ---
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/project-talan/talan-core/latest/install.sh)"
-sudo tln install:linux java,maven,node-9.x,docker,docker-compose
+sudo tln install java,maven,docker,docker-compose
 
 echo --- Install Angular CLI ---
-sudo tln install angular-cli
+sudo npm install -g @angular/cli
 
 echo --- Check cmponents ---
 java -version
